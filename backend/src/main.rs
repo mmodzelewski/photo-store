19876db84f1b8f614784373b170c1fd632ede168
Maciej Modzelewski <maciej@modzelewski.dev>
2023-05-16 19:54:02 +0200

    Implement example of sending object to store

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::Path;
+
+use aws_sdk_s3::primitives::ByteStream;
+use axum::{routing::get, Router};
+
+const URL: &str = "";
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(get_data));
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn get_data<'a>() -> &'a str {
+    let config = aws_config::from_env()
+        .region("auto")
+        .endpoint_url(URL)
+        .load()
+        .await;
+    let client = aws_sdk_s3::Client::new(&config);
+    let result = client.list_buckets().send().await;
+    println!("{:?}", result);
+
+    let body = ByteStream::from_path(Path::new("")).await;
+    let result = client
+        .put_object()
+        .bucket("photo-store-test")
+        .key("test-image")
+        .content_type("image/jpeg")
+        .body(body.unwrap())
+        .send()
+        .await;
+
+    println!("{:?}", result);
+    return "hello data";
 }

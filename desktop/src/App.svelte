79a0a2557f50a6cdedf496432e254cd33355d994
Maciej Modzelewski <maciej@modzelewski.dev>
2023-06-21 06:28:02 +0200

    Add dialog for selecting a directory

--- desktop/src/App.svelte
+++ desktop/src/App.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
-  import Photos from './lib/Photos.svelte'
+    import { open } from "@tauri-apps/api/dialog";
+
+    async function selectDir() {
+        await open({
+            directory: true,
+            recursive: true,
+        });
+    }
 </script>
 
 <main class="container">
-  <h1>Photo store</h1>
+    <h1>Photo store</h1>
 
-  <div class="row">
-    <Photos />
-  </div>
-
+    <div class="row">
+        <button on:click={selectDir}>Select directory</button>
+    </div>
 </main>
-

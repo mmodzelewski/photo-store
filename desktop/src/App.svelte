183ccbb4b55971ce4e88838f0ac7c2794667c74d
Maciej Modzelewski <maciej@modzelewski.dev>
2023-06-21 07:23:56 +0200

    Add basic styling

--- desktop/src/App.svelte
+++ desktop/src/App.svelte
@@ -9,10 +9,15 @@
     }
 </script>
 
-<main>
+<div class="container mx-auto p-4 text-white">
     <h1>Photo store</h1>
 
     <div>
-        <button on:click={selectDir}>Select directory</button>
+        <button
+            on:click={selectDir}
+            class="border-solid border-2 border-zinc-500 p-4 rounded-lg bg-zinc-700"
+        >
+            Select directory
+        </button>
     </div>
-</main>
+</div>

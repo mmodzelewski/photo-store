f11534287af9c8bebd90d3d4703b7152f79e7b30
Maciej Modzelewski <maciej@modzelewski.dev>
2023-06-25 14:06:28 +0200

    Extend navigation store

--- desktop/src/App.svelte
+++ desktop/src/App.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
     import { navigation } from "./navigation";
-    import GalleryPage from "./pages/GalleryPage.svelte";
     import Button from "./components/Button.svelte";
 </script>
 
 <div class="container mx-auto p-4 text-white">
     <h1>Photo store</h1>
-    <Button on:click={() => navigation.set(GalleryPage)}>Go to gallery</Button>
+    <Button on:click={() => navigation.goToGallery()}>Go to gallery</Button>
 
     <svelte:component this={$navigation} />
 </div>
--- desktop/src/navigation.ts
+++ desktop/src/navigation.ts
@@ -1,4 +1,15 @@
 import { writable } from "svelte/store";
 import IntroPage from "./pages/IntroPage.svelte";
+import GalleryPage from "./pages/GalleryPage.svelte";
 
-export const navigation = writable(IntroPage);
+function createNavigation() {
+    const { subscribe, set } = writable(IntroPage);
+
+    return {
+        subscribe,
+        goToGallery: () => set(GalleryPage),
+    };
+}
+
+export const navigation = createNavigation();
+

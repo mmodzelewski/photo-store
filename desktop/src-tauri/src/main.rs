d1adbeb876982630ee9e9fff91543f6ae4329383
Maciej Modzelewski <maciej@modzelewski.dev>
2023-07-01 16:58:15 +0200

    Add sqlite connection to desktop app

--- desktop/src-tauri/src/main.rs
+++ desktop/src-tauri/src/main.rs
@@ -4,6 +4,8 @@
 use std::fs;
 
 use reqwest::multipart::Part;
+use rusqlite::Connection;
+use tauri::App;
 
 #[tauri::command]
 async fn send_image() {
@@ -12,14 +14,14 @@ async fn send_image() {
     let path = dir.next().unwrap().unwrap().path();
     let file = fs::read(path).unwrap();
 
-     let form = reqwest::multipart::Form::new().part(
-         "file",
-         Part::bytes(file)
-             .file_name("test.jpg")
-             .mime_str("image/jpeg")
-             .unwrap(),
-     );
-     let client = reqwest::Client::new();
+    let form = reqwest::multipart::Form::new().part(
+        "file",
+        Part::bytes(file)
+            .file_name("test.jpg")
+            .mime_str("image/jpeg")
+            .unwrap(),
+    );
+    let client = reqwest::Client::new();
     let res = client
         .post("http://localhost:3000/upload")
         .multipart(form)
@@ -31,6 +33,24 @@ async fn send_image() {
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![send_image])
+        .setup(|app| {
+            init_db(&app);
+            return Ok(());
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+fn init_db(app: &App) {
+    let path = app.path_resolver().app_data_dir().unwrap();
+
+    let conn = Connection::open(path.join("data.db3")).unwrap();
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS image (
+            id INTEGER PRIMARY KEY,
+            path TEXT NOT NULL
+        )",
+        (),
+    )
+    .unwrap();
 }

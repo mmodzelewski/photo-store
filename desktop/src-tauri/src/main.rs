4cc563577d154ed4537738eeb6e4881f5c705c00
Maciej Modzelewski <maciej@modzelewski.dev>
2023-07-02 12:41:02 +0200

    Add custom errors

--- desktop/src-tauri/src/main.rs
+++ desktop/src-tauri/src/main.rs
@@ -12,6 +12,31 @@ struct Database {
     connection: Mutex<Connection>,
 }
 
+#[derive(Debug, thiserror::Error)]
+enum Error {
+    #[error(transparent)]
+    IO(#[from] std::io::Error),
+    #[error(transparent)]
+    DB(#[from] rusqlite::Error),
+    #[error(transparent)]
+    DBMigrations(#[from] rusqlite_migration::Error),
+    #[error("{0}")]
+    Generic(String),
+    #[error("Runtime error: {0}")]
+    Runtime(String),
+}
+
+type Result<T> = std::result::Result<T, Error>;
+
+impl serde::Serialize for Error {
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: serde::ser::Serializer,
+    {
+        serializer.serialize_str(self.to_string().as_ref())
+    }
+}
+
 #[tauri::command]
 async fn send_image() {
     println!("send_image called");
@@ -36,34 +61,38 @@ async fn send_image() {
 }
 
 #[tauri::command]
-fn save_images_dirs(dirs: Vec<&str>, database: tauri::State<Database>) {
-    let mut conn = database.connection.lock().unwrap();
+fn save_images_dirs(dirs: Vec<&str>, database: tauri::State<Database>) -> Result<()> {
+    let mut conn = database
+        .connection
+        .lock()
+        .map_err(|err| Error::Generic(err.to_string()))?;
 
-    let tx = conn.transaction().unwrap();
+    let tx = conn.transaction()?;
     {
-        let mut stmt = tx
-            .prepare("INSERT INTO directory (path) VALUES (?1)")
-            .unwrap();
-        dirs.iter().for_each(|dir| {
-            stmt.execute([dir]).unwrap();
-        });
+        let mut stmt = tx.prepare("INSERT INTO directory (path) VALUES (?1)")?;
+        dirs.iter()
+            .map(|dir| stmt.execute([dir]))
+            .collect::<std::result::Result<Vec<_>, _>>()?;
     }
-    tx.commit().unwrap();
+    tx.commit()?;
+    return Ok(());
 }
 
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![send_image, save_images_dirs])
         .setup(|app| {
-            init_db(&app);
+            init_db(&app)?;
             return Ok(());
         })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
 
-fn init_db(app: &App) {
-    let path = app.path_resolver().app_data_dir().unwrap();
+fn init_db(app: &App) -> Result<()> {
+    let path = app.path_resolver().app_data_dir().ok_or(Error::Runtime(
+        "Could not get app data directory".to_owned(),
+    ))?;
 
     let migrations = Migrations::new(vec![M::up(
         "CREATE TABLE directory (
@@ -71,11 +100,12 @@ fn init_db(app: &App) {
             path TEXT NOT NULL UNIQUE ON CONFLICT IGNORE
         );",
     )]);
-    let mut conn = Connection::open(path.join("data.db3")).unwrap();
+    let mut conn = Connection::open(path.join("data.db3"))?;
     // conn.pragma_update(None, "journal_mode", &"WAL").unwrap(); // verify
-    migrations.to_latest(&mut conn).unwrap();
+    migrations.to_latest(&mut conn)?;
 
     app.manage(Database {
         connection: Mutex::new(conn),
     });
+    return Ok(());
 }

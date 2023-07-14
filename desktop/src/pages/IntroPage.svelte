b75f8e90cd93d3159474d239652526ea5f9f78bd
Maciej Modzelewski <maciej@modzelewski.dev>
2023-07-14 20:02:11 +0200

    Update the intro page to display processed images while indexing

--- desktop/src/pages/IntroPage.svelte
+++ desktop/src/pages/IntroPage.svelte
@@ -7,27 +7,52 @@
     import { navigation } from "../navigation";
     import { onDestroy, onMount } from "svelte";
 
+    type ThumbnailsGeneratedPayload = {
+        done: number;
+        total: number;
+        latest: string;
+    };
+    type FilesIndexedPayload = { total: number };
+
     const unlistenFunctions = [];
     let dialog: HTMLDialogElement;
     let filesIndexed: number;
-    let thumbnailsGenerated: { done: number; total: number };
+    let thumbnailsGenerated: ThumbnailsGeneratedPayload;
+    let latestThumbnail: string;
+    let thumbnailToDisplay: string;
+    let interval: NodeJS.Timer;
+
     onMount(async () => {
-        const unlisten1 = await listen("thumbnails-generated", (event) => {
-            console.log(event.payload);
-            thumbnailsGenerated = event.payload;
-        });
+        const unlisten1 = await listen<ThumbnailsGeneratedPayload>(
+            "thumbnails-generated",
+            (event) => {
+                thumbnailsGenerated = event.payload;
+                latestThumbnail = convertFileSrc(thumbnailsGenerated.latest);
+                if (!interval) {
+                    thumbnailToDisplay = latestThumbnail;
+                    interval = setInterval(() => {
+                        thumbnailToDisplay = latestThumbnail;
+                    }, 2000);
+                }
+            }
+        );
         unlistenFunctions.push(unlisten1);
 
-        const unlisten2 = await listen("files-indexed", (event) => {
-            console.log(event.payload);
-            filesIndexed = event.payload.total;
-            dialog.showModal();
-        });
+        const unlisten2 = await listen<FilesIndexedPayload>(
+            "files-indexed",
+            (event) => {
+                filesIndexed = event.payload.total;
+                dialog.showModal();
+            }
+        );
         unlistenFunctions.push(unlisten2);
     });
 
     onDestroy(() => {
         unlistenFunctions.forEach((unlisten) => unlisten());
+        if (interval) {
+            clearInterval(interval);
+        }
     });
 
     async function selectDir() {
@@ -43,10 +68,6 @@
             navigation.goToGallery();
         }
     }
-
-    function getImgSrc(status) {
-        return convertFileSrc(status.latest);
-    }
 </script>
 
 <div>
@@ -54,16 +75,20 @@
 </div>
 <dialog
     bind:this={dialog}
-    class="h-48 w-96 rounded-lg border-2 border-solid border-zinc-500 bg-zinc-700 text-white"
+    class="h-44 w-96 rounded-lg border-2 border-solid border-zinc-500 bg-zinc-700 text-white"
 >
     {#if filesIndexed === undefined}
         <div>Indexing images</div>
     {:else if thumbnailsGenerated === undefined}
         <div>Generating thumbnails</div>
     {:else}
-        <div>
-            Generating thumbnails {thumbnailsGenerated.done} / {thumbnailsGenerated.total}
-            <img src={getImgSrc(thumbnailsGenerated)} />
+        <div class="flex h-full items-center justify-around align-middle">
+            <p>
+                {thumbnailsGenerated.done} / {thumbnailsGenerated.total}
+            </p>
+            <div class="h-36 w-36 overflow-hidden">
+                <img src={thumbnailToDisplay} />
+            </div>
         </div>
     {/if}
 </dialog>

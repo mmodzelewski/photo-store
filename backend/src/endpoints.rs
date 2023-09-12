86f1868e491db8a9ca547ef9be4735f413ca2678
Maciej Modzelewski <maciej@modzelewski.dev>
2023-09-12 08:18:25 +0200

    Add database pool to the state, create new endpoint for uploading files

---
+++ backend/src/endpoints.rs
@@ -0,0 +1,153 @@
+use std::path::Path;
+
+use aws_sdk_s3::{types::{CompletedPart, CompletedMultipartUpload}, primitives::ByteStream};
+use axum::extract::Multipart;
+
+use crate::config::Config;
+
+pub(crate) async fn list_uploads() -> &'static str {
+    let local_config = Config::load().await;
+    if local_config.is_err() {
+        println!("{:?}", local_config);
+        return "error";
+    }
+    let local_config = local_config.unwrap();
+
+    let config = aws_config::from_env()
+        .region("auto")
+        .endpoint_url(local_config.r2_url)
+        .load()
+        .await;
+    let client = aws_sdk_s3::Client::new(&config);
+
+    let result = client
+        .list_multipart_uploads()
+        .bucket(&local_config.bucket_name)
+        .send()
+        .await
+        .unwrap();
+
+    for upload in result.uploads().unwrap() {
+        println!("{:?}", upload);
+        client
+            .abort_multipart_upload()
+            .bucket(&local_config.bucket_name)
+            .key(upload.key().unwrap())
+            .upload_id(upload.upload_id().unwrap())
+            .send()
+            .await
+            .unwrap();
+    }
+
+    return "hello data";
+}
+
+pub(crate) async fn upload(mut multipart: Multipart) {
+    let local_config = Config::load().await;
+    if local_config.is_err() {
+        println!("{:?}", local_config);
+        return;
+    }
+    let local_config = local_config.unwrap();
+    let key = "test-image2";
+
+    let config = aws_config::from_env()
+        .region("auto")
+        .endpoint_url(local_config.r2_url)
+        .load()
+        .await;
+    let client = aws_sdk_s3::Client::new(&config);
+    let output = client
+        .create_multipart_upload()
+        .bucket(&local_config.bucket_name)
+        .key(key)
+        .content_type("image/jpeg")
+        .send()
+        .await
+        .unwrap();
+    let upload_id = output.upload_id().unwrap();
+
+    let mut part_number = 1;
+    let mut upload_parts: Vec<CompletedPart> = Vec::new();
+
+    while let Some(field) = multipart.next_field().await.unwrap() {
+        let name = field.name().unwrap().to_string();
+        let file_name = field.file_name().unwrap().to_string();
+        let content_type = field.content_type().unwrap().to_string();
+        let data = field.bytes().await.unwrap();
+
+        println!(
+            "Length of `{}` (`{}`: `{}`) is {} bytes",
+            name,
+            file_name,
+            content_type,
+            data.len()
+        );
+
+        let stream = ByteStream::from(data);
+
+        let completed = client
+            .upload_part()
+            .key(key)
+            .bucket(&local_config.bucket_name)
+            .upload_id(upload_id)
+            .body(stream)
+            .part_number(part_number)
+            .send()
+            .await
+            .unwrap();
+
+        upload_parts.push(
+            CompletedPart::builder()
+                .e_tag(completed.e_tag().unwrap_or_default())
+                .part_number(part_number)
+                .build(),
+        );
+        part_number += 1;
+    }
+    let completed = CompletedMultipartUpload::builder()
+        .set_parts(Some(upload_parts))
+        .build();
+
+    let complete = client
+        .complete_multipart_upload()
+        .bucket(&local_config.bucket_name)
+        .key(key)
+        .multipart_upload(completed)
+        .upload_id(upload_id)
+        .send()
+        .await
+        .unwrap();
+    dbg!(complete);
+}
+
+pub(crate) async fn get_data() -> &'static str {
+    let local_config = Config::load().await;
+    if local_config.is_err() {
+        println!("{:?}", local_config);
+        return "error";
+    }
+    let local_config = local_config.unwrap();
+
+    let config = aws_config::from_env()
+        .region("auto")
+        .endpoint_url(local_config.r2_url)
+        .load()
+        .await;
+    let client = aws_sdk_s3::Client::new(&config);
+    let result = client.list_buckets().send().await;
+    println!("{:?}", result);
+
+    let body = ByteStream::from_path(Path::new("")).await;
+    let result = client
+        .put_object()
+        .bucket(local_config.bucket_name)
+        .key("test-image")
+        .content_type("image/jpeg")
+        .body(body.unwrap())
+        .send()
+        .await;
+
+    println!("{:?}", result);
+    return "hello data";
+}
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,20 +1,19 @@
 mod config;
+mod endpoints;
 
-use std::path::Path;
-
-use aws_sdk_s3::{
-    primitives::ByteStream,
-    types::{CompletedMultipartUpload, CompletedPart},
-};
 use axum::{
-    extract::{DefaultBodyLimit, Multipart},
+    extract::{DefaultBodyLimit, Json, Path, State},
     routing::{get, post},
     Router,
 };
+use endpoints::{get_data, list_uploads, upload};
 use sqlx::postgres::PgPoolOptions;
 use tower_http::limit::RequestBodyLimitLayer;
 
-use crate::config::Config;
+#[derive(Clone)]
+struct AppState {
+    db: sqlx::Pool<sqlx::Postgres>,
+}
 
 #[tokio::main]
 async fn main() {
@@ -24,168 +23,38 @@ async fn main() {
         .await
         .unwrap();
 
-    let row: (i64,) = sqlx::query_as("SELECT $1")
-        .bind(150_i64)
-        .fetch_one(&pool)
-        .await
-        .unwrap();
-
-    println!("{:?}", row);
-
     let app = Router::new()
         .route("/", get(get_data))
         .route("/upload", post(upload).get(list_uploads))
+        .route("/u/:id/files", post(file_meta_upload))
         .layer(DefaultBodyLimit::disable())
-        .layer(RequestBodyLimitLayer::new(250 * 1024 * 1024));
+        .layer(RequestBodyLimitLayer::new(250 * 1024 * 1024))
+        .with_state(AppState { db: pool });
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
 
-async fn list_uploads() -> &'static str {
-    let local_config = Config::load().await;
-    if local_config.is_err() {
-        println!("{:?}", local_config);
-        return "error";
-    }
-    let local_config = local_config.unwrap();
-
-    let config = aws_config::from_env()
-        .region("auto")
-        .endpoint_url(local_config.r2_url)
-        .load()
-        .await;
-    let client = aws_sdk_s3::Client::new(&config);
-
-    let result = client
-        .list_multipart_uploads()
-        .bucket(&local_config.bucket_name)
-        .send()
-        .await
-        .unwrap();
-
-    for upload in result.uploads().unwrap() {
-        println!("{:?}", upload);
-        client
-            .abort_multipart_upload()
-            .bucket(&local_config.bucket_name)
-            .key(upload.key().unwrap())
-            .upload_id(upload.upload_id().unwrap())
-            .send()
-            .await
-            .unwrap();
-    }
-
-    return "hello data";
+#[derive(serde::Deserialize)]
+struct NewFile {
+    name: String,
+    uuid: uuid::Uuid,
 }
 
-async fn upload(mut multipart: Multipart) {
-    let local_config = Config::load().await;
-    if local_config.is_err() {
-        println!("{:?}", local_config);
-        return;
-    }
-    let local_config = local_config.unwrap();
-    let key = "test-image2";
-
-    let config = aws_config::from_env()
-        .region("auto")
-        .endpoint_url(local_config.r2_url)
-        .load()
-        .await;
-    let client = aws_sdk_s3::Client::new(&config);
-    let output = client
-        .create_multipart_upload()
-        .bucket(&local_config.bucket_name)
-        .key(key)
-        .content_type("image/jpeg")
-        .send()
+async fn file_meta_upload(
+    State(state): State<AppState>,
+    Path(id): Path<String>,
+    Json(file): Json<NewFile>,
+) {
+    println!("{}", id);
+    println!("{}", file.name);
+    println!("{}", file.uuid);
+    let db = state.db;
+    let count = sqlx::query!("SELECT count(1) FROM file WHERE uuid = $1", &file.uuid)
+        .fetch_one(&db)
         .await
         .unwrap();
-    let upload_id = output.upload_id().unwrap();
-
-    let mut part_number = 1;
-    let mut upload_parts: Vec<CompletedPart> = Vec::new();
-
-    while let Some(field) = multipart.next_field().await.unwrap() {
-        let name = field.name().unwrap().to_string();
-        let file_name = field.file_name().unwrap().to_string();
-        let content_type = field.content_type().unwrap().to_string();
-        let data = field.bytes().await.unwrap();
-
-        println!(
-            "Length of `{}` (`{}`: `{}`) is {} bytes",
-            name,
-            file_name,
-            content_type,
-            data.len()
-        );
-
-        let stream = ByteStream::from(data);
-
-        let completed = client
-            .upload_part()
-            .key(key)
-            .bucket(&local_config.bucket_name)
-            .upload_id(upload_id)
-            .body(stream)
-            .part_number(part_number)
-            .send()
-            .await
-            .unwrap();
-
-        upload_parts.push(
-            CompletedPart::builder()
-                .e_tag(completed.e_tag().unwrap_or_default())
-                .part_number(part_number)
-                .build(),
-        );
-        part_number += 1;
-    }
-    let completed = CompletedMultipartUpload::builder()
-        .set_parts(Some(upload_parts))
-        .build();
-
-    let complete = client
-        .complete_multipart_upload()
-        .bucket(&local_config.bucket_name)
-        .key(key)
-        .multipart_upload(completed)
-        .upload_id(upload_id)
-        .send()
-        .await
-        .unwrap();
-    dbg!(complete);
-}
-
-async fn get_data() -> &'static str {
-    let local_config = Config::load().await;
-    if local_config.is_err() {
-        println!("{:?}", local_config);
-        return "error";
-    }
-    let local_config = local_config.unwrap();
-
-    let config = aws_config::from_env()
-        .region("auto")
-        .endpoint_url(local_config.r2_url)
-        .load()
-        .await;
-    let client = aws_sdk_s3::Client::new(&config);
-    let result = client.list_buckets().send().await;
-    println!("{:?}", result);
-
-    let body = ByteStream::from_path(Path::new("")).await;
-    let result = client
-        .put_object()
-        .bucket(local_config.bucket_name)
-        .key("test-image")
-        .content_type("image/jpeg")
-        .body(body.unwrap())
-        .send()
-        .await;
-
-    println!("{:?}", result);
-    return "hello data";
+    let count = count.count.unwrap_or(0);
+    println!("{}", count);
 }
